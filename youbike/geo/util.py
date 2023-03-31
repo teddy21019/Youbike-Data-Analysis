e8883bc2ab400c130d8819f5f58c2aa51212b913
Ted <teddychenster@gmail.com>
2023-04-01 00:21:11 +0800

    Fix import relatice bug

--- youbike/geo/util.py
+++ youbike/geo/util.py
@@ -1,9 +1,9 @@
 import pandas as pd
+import geo.geo_info
 
 def add_coordinate(youbike_df:pd.DataFrame)-> pd.DataFrame:
     """ Adds coordinate info to the data"""
-    import geo_info
-    geo_df = geo_info.load_geoinfo()
+    geo_df = geo.geo_info.load_geoinfo()
 
     merged_data = youbike_df.merge(geo_df, left_on='rent_station', right_on='sna') \
         .merge(geo_df, left_on='return_station', right_on='sna', suffixes=('_rent', '_return'))
